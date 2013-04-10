94535c89db51d744dc184d63eca164aa6b8db678
Matt Brewer <matt.brewer@me.com>
2013-04-10 05:51:56 -0400

    IndexAsBlog now renders a proc passed to title/body methods in view context

--- lib/active_admin/view_helpers/method_or_proc_helper.rb
+++ lib/active_admin/view_helpers/method_or_proc_helper.rb
@@ -76,7 +76,7 @@ module MethodOrProcHelper
     end
   end
 
-  # This method is different from the others in that it calls `instance_eval` on the reciever,
+  # This method is different from the others in that it calls `instance_exec` on the reciever,
   # passing it the proc. This evaluates the proc in the context of the reciever, thus changing
   # what `self` means inside the proc.
   def render_in_context(context, obj)
